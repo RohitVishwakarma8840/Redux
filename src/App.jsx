34c668fff29ef5a74fc22ea45606f870547b3958
Rohit <rohit8840185@gmail.com>
2025-01-05 15:07:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <div className='m-auto container p-10 mt-5 bg-red-300'>
+
+      <h1 className='text-3xl text-center font-bold text-red-900'>
+        Let's Start Redux Toolkit
+      </h1>
+
+
+      </div>
+
+
+
+
+
+       
+    </>
+  )
+}
+
+export default App
