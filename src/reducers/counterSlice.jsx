05f2283295cb5e847198c7da657ad7ad790e9b98
Rohit <rohit8840185@gmail.com>
2025-01-06 18:52:14 +0530

    Adding code redux

---
+++ src/reducers/counterSlice.jsx
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  value: 0,
+};
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      console.log(action);
+      state.value += action.payload;
+    },
+  },
+});
+
+// Export the actions for use in components
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+// Export the reducer as default
+export default counterSlice.reducer;
